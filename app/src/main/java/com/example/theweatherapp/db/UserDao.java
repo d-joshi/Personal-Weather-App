8d60f3b5e5a3209b9c8b0987fd1c22104c61a2a4
Sean McNally <seanmcnally@outlook.com>
2020-03-18 18:56:17 -0400

    Add user DAO

---
+++ app/src/main/java/com/example/theweatherapp/db/UserDao.java
@@ -0,0 +1,28 @@
+package com.example.theweatherapp.db;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface UserDao {
+    @Query("SELECT * FROM users ORDER BY rowid")
+    LiveData<List<User>> getAll();
+
+    @Insert
+    void insert(User... users);
+
+    @Update
+    void update(User... user);
+
+    @Delete
+    void delete(User... user);
+
+    @Query("DELETE FROM users WHERE rowid = :userid")
+    void delete(int userid);
+}
