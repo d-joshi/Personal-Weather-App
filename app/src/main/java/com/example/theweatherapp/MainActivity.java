2a718755dfa092342868597ea7e18878ee96408c
Sean McNally <seanmcnally@outlook.com>
2020-04-21 16:12:35 -0400

    Merge branch 'master' of C:\Users\SeanM\AndroidStudioProjects\Personal-Weather-App with conflicts.

--- app/src/main/java/com/example/theweatherapp/MainActivity.java
+++ app/src/main/java/com/example/theweatherapp/MainActivity.java
@@ -3,39 +3,71 @@ package com.example.theweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.Spinner;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.EditText;
+
+
+import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     private static String forecastDaysNum = "3";
     String city = "Philadelphia, PA";
 
-    private RadioButton unit;
-    private Spinner verbosity;
-    private EditText lowTemp;
-    private EditText midTemp;
-    private EditText highTemp;
-    private Button save;
-
     public static final String SHARED_PREFS = "";
+
+    String CITY = "philadelphia,pa,us";
+    String API = "2e623cf734abdaf0dccd465fdbdd49c2";
+    String UNITS = "metric";
+
+    TextView tempTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.activity_main);
         setContentView(R.layout.forecast);
 
-        unit = (RadioButton) findViewById(R.id.unit);
-        verbosity = (Spinner) findViewById(R.id.verbosity);
-        lowTemp = (EditText) findViewById(R.id.lowTemp);
-        midTemp = (EditText) findViewById(R.id.midTemp);
-        highTemp = (EditText) findViewById(R.id.highTemp);
-        save = (Button) findViewById(R.id.save);
+        Log.d("onCreate", "onCreate");
+
+        tempTxt = findViewById(R.id.temp);
+    }
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        /*@Override
+        protected void onPreExcecute() {
+            super.onPreExecute();
+
+
+        }*/
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=" + UNITS + "&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try{
+                Log.d("onPostExecute", "onPostExecute");
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject main = jsonObject.getJSONObject("main");
+
+                String temp = main.getString("temp") + "°C";
+
+                tempTxt.setText(temp);
+            }
+            catch (JSONException e) {
+                Log.d("exception", "¯\\_(ツ)_/¯");
+            }
+        }
     }
 }
+
+//created with help from this tutorial: https://www.survivingwithandroid.com/android-openweathermap-app-weather-app/
