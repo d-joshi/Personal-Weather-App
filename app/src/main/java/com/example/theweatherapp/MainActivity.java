278d6b6c3ada9b32db5093884dfb4db24d97d0d6
Dhruvil <dhruvilj35@gmail.com>
2020-04-21 19:57:37 -0400

    intent

--- app/src/main/java/com/example/theweatherapp/MainActivity.java
+++ app/src/main/java/com/example/theweatherapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.theweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText midTemp;
     private EditText highTemp;
     private Button save;
+    private  ImageButton settings_b;
 
     public static final String SHARED_PREFS = "";
 
@@ -30,12 +33,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forecast);
-
         unit = (RadioButton) findViewById(R.id.unit);
         verbosity = (Spinner) findViewById(R.id.verbosity);
         lowTemp = (EditText) findViewById(R.id.lowTemp);
         midTemp = (EditText) findViewById(R.id.midTemp);
         highTemp = (EditText) findViewById(R.id.highTemp);
         save = (Button) findViewById(R.id.save);
+        settings_b = (ImageButton) findViewById(R.id.Settings_button);
+
+        settings_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, SettingsActivity.class);
+            }
+        });
+
+
+
     }
 }
