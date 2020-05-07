687a4cace2a1823ceb7d9cfd7df624924895e0dd
Dhruvil Joshi <dhruvilj35@gmail.com>
2020-05-07 11:51:49 -0400

    QA

--- app/src/main/java/com/example/theweatherapp/LocationActivity.java
+++ app/src/main/java/com/example/theweatherapp/LocationActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
@@ -15,7 +16,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -153,7 +153,7 @@ public class LocationActivity extends AppCompatActivity {
                             textView1.setText(Html.fromHtml("<font color ='#6200EE'><b>Latitude :</b><br></font>" + addresses.get(0).getLatitude())); //get latitude
                             textView2.setText(Html.fromHtml("<font color ='#6200EE'><b>Longitude :</b><br></font>" + addresses.get(0).getLongitude())); //get longitude
                             textView3.setText(Html.fromHtml("<font color ='#6200EE'><b>Country Name :</b><br></font>" + addresses.get(0).getCountryName())); //get country name
-                            textView4.setText(Html.fromHtml("<font color ='#6200EE'><b>Locality :</b><br></font>" + addresses.get(0).getLocality())); //get Locality
+                            textView4.setText(Html.fromHtml("<font color ='#6200EE'><b>City :</b><br></font>" + addresses.get(0).getLocality())); //get City
                             textView5.setText(Html.fromHtml("<font color ='#6200EE'><b>Address :</b><br></font>" + addresses.get(0).getAddressLine(0))); //get Locality
 
                         } catch (IOException e) {
